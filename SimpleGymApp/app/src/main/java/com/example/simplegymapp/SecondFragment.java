5c0d0789ff3805c68c22fbf1827ad35dbc2deba0
@steini96 <steini.tki@gmail.com>
2022-03-27 09:51:33 +0000

    Create ListFragment

--- SimpleGymApp/app/src/main/java/com/example/simplegymapp/SecondFragment.java
+++ SimpleGymApp/app/src/main/java/com/example/simplegymapp/SecondFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,11 +17,18 @@ public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
 
+    // Array of strings...
+    ListView simpleList;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        super.onCreate(savedInstanceState);
+
+
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -36,6 +45,14 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+        simpleList = (ListView)getView().findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, countryList);
+
+            // new ArrayAdapter<String>(this, R.layout.activity_main, R.id.textView, android.R.layout.simple_list_item_1);
+
+        simpleList.setAdapter(arrayAdapter);
+
     }
 
     @Override
