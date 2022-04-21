eda18fd9233d30541394b1a7a18bd4efd73d69a3
tfj2 <tfj2@hi.is>
2022-04-21 14:16:25 +0000

    userinfo meira

--- SimpleGymApp/app/src/main/java/com/example/simplegymapp/UserInfoFragment.java
+++ SimpleGymApp/app/src/main/java/com/example/simplegymapp/UserInfoFragment.java
@@ -2,11 +2,14 @@ package com.example.simplegymapp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -27,6 +30,11 @@ import java.util.List;
 
 
 public class UserInfoFragment  extends Fragment {
+    private String email, password, workoutFreq;
+    private Difficulty workoutDiff;
+    private  UserFitnessInfo userFitnessInfo1 = new UserFitnessInfo(2, WorkoutType.RUNNING, Difficulty.EASY);
+    private UserFitnessInfo userFitnessInfo2 = new UserFitnessInfo();
+
 
     private UserinfoBinding binding;
 
@@ -60,9 +68,9 @@ public class UserInfoFragment  extends Fragment {
             }
         });
 
-        simpleList = (ListView)getView().findViewById(R.id.simpleListView);
 
-        UserFitnessInfo userFitnessInfo1 = new UserFitnessInfo(2, WorkoutType.RUNNING, Difficulty.EASY);
+
+        simpleList = (ListView)getView().findViewById(R.id.simpleListView);
 
         list.add(userFitnessInfo1);
         Object[] exArray = list.toArray();
@@ -72,6 +80,37 @@ public class UserInfoFragment  extends Fragment {
         // new ArrayAdapter<String>(this, R.layout.activity_main, R.id.textView, android.R.layout.simple_list_item_1);
 
         simpleList.setAdapter(arrayAdapter);
+        binding.button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                RadioGroup radioGroup = (RadioGroup) getView().findViewById(R.id.radioGroup);
+                        switch (radioGroup.getCheckedRadioButtonId()) {
+                            case R.id.radioButton6:
+                                workoutDiff = Difficulty.EASY;
+                                break;
+                            case R.id.radioButton7:
+                                workoutDiff = Difficulty.MEDIUM;
+                                break;
+                            case R.id.radioButton8:
+                                workoutDiff = Difficulty.HARD;
+                                break;
+                        }
+                EditText editEmail = (EditText) getView().findViewById(R.id.editTextTextEmailAddress2);
+                email = editEmail.getText().toString();
+                EditText editPassword = (EditText) getView().findViewById(R.id.editTextTextPassword2);
+                password = editPassword.getText().toString();
+                EditText editWorkoutFreq = (EditText) getView().findViewById(R.id.editTextNumber);
+                workoutFreq = editWorkoutFreq.getText().toString();
+                Log.d("email", email);
+                Log.d("password", password);
+                Log.d("workoutFreq", workoutFreq);
+                Log.d("workoutDiff", "" + workoutDiff);
+
+                ArrayAdapter<UserFitnessInfo> arrayAdapter = new ArrayAdapter<UserFitnessInfo>(getActivity(), android.R.layout.simple_list_item_1, list);
+                NavHostFragment.findNavController(UserInfoFragment.this)
+                        .navigate(R.id.userInfo_to_first_fragment);
+            }
+        });
 
     }
 
